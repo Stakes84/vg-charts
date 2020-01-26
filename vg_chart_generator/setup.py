60e1d86969bb860690cd59158e25cba8bf55fd83
Anthony LaRocca <alarocca@APX2183.local>
2020-01-25 19:09:14 -0800

    First pass at an installable library

---
+++ vg_chart_generator/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Video Game Chart Generator',
+    'author': 'Anthony LaRocca',
+    'url': 'https://github.com/Stakes84/vg-charts',
+    'author_email': 'zensky@gmail.com',
+    'version': '1.0.0',
+    'install_requires': ['argparse',
+                         'matplotlib',
+                         'pandas'],
+    'packages': ['vg_chart_generator'],
+    # 'package_data':{ 'vg_chart_generator': ['*.csv']},
+    'include_package_data': True,
+    'name': 'vg_chart_generator',
+    'entry_points': {'console_scripts': ['vg-chart-generator=vg_chart_generator:main']}
+}
+
+setup(**config)
